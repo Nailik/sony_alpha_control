3ec3ca37086af7465df76b47ad634506f55a9fd1
Kilian Eller <kilianeller12@outlook.com>
2020-10-07 16:30:06 +0200

    updates for windows version

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -4,8 +4,8 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <file_chooser_plugin.h>
-#include <flutter_usb_plugin.h>
+#include <file_chooser/file_chooser_plugin.h>
+#include <flutter_usb/flutter_usb_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileChooserPluginRegisterWithRegistrar(
